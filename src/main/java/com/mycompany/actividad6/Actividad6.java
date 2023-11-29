64f84431a13add16475a1e8d9ef8ae2dbb81a16c
Sebp0r <juposadar@unal.edu.co>
2023-11-28 21:26:25 -0500

    Act6
    Trabajo CRUD completo

---
+++ src/main/java/com/mycompany/actividad6/Actividad6.java
@@ -0,0 +1,12 @@
+package com.mycompany.actividad6;
+/**
+ *
+ * @author Sebastian Posada
+ */
+public class Actividad6 {
+
+    public static void main(String[] args) {
+        Form frm = new Form();
+        frm.setVisible(true);
+    }
+}
